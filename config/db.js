f5f45ca4383c449772bc3c1f6d2635c303013e14
Cahya Dinar Prastyo <cahyadinar241@gmail.com>
2022-04-29 05:58:34 +0700

    fixing deployment

--- config/db.js
+++ config/db.js
@@ -5,7 +5,10 @@ const db = new Pool({
   host: process.env.DB_HOST || 'localhost',
   database: process.env.DB_NAME || 'finalproject1_injs',
   password: process.env.DB_PASSWORD || 'postgres',
-  port: process.env.DB_PORT || '5432'
+  port: process.env.DB_PORT || '5432',
+  ssl: {
+    rejectUnauthorized: false,
+  }
 });
 
 module.exports = db;
